33129e431f3d5b9b39b226be4c443bd24b5e4c1c
Anatolyaaa <30937754+Anatolyaaa@users.noreply.github.com>
2017-08-12 11:30:16 +0300

    Update about.php

--- about.php
+++ about.php
@@ -1,15 +1,12 @@
 <?php
-namespace знакомство c php;
-?>
-<?php
-echo 'Имя	Толя';
+echo 'Имя	                       Толя';
 echo PHP_EOL;
-echo 'Возраст	32';
+echo 'Возраст                        23';
 echo PHP_EOL;
-echo 'Адрес электронной почты	href="Mitroshin_tolya@mail.com"';
+echo 'Адрес электронной почты        Mitroshin_tolya@mail.com';
 echo PHP_EOL;
-echo 'Город	Казань';
+echo 'Город	                       Казань';
 echo PHP_EOL;
-echo 'О себе Пока гораздо больше веб-дизайнер нежели разработчик';
+echo 'О себе                         Пока гораздо больше веб-дизайнер нежели разработчик';
 echo PHP_EOL;
 ?> 
